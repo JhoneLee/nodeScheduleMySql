8a2bc713f924eb0687c405a1b81e52fa10e33f91
liyunjiao2048@163.com <liyunjiao@baidu.com>
2018-07-11 17:05:41 +0800

    init

--- config/connect.js
+++ config/connect.js
@@ -2,13 +2,13 @@
 * @Author: liyunjiao2048@163.com
 * @Date:   2018-07-11 16:11:37
 * @Last Modified by:   liyunjiao2048@163.com
-* @Last Modified time: 2018-07-11 16:27:34
+* @Last Modified time: 2018-07-11 17:05:25
 */
 
 module.exports = {
-    host:'10.117.140.34',  // ip也行域名也行
+    host:'xxxxxxxxxxxxx',  // ip也行域名也行
     user:'root',
-    password:'19880923',
+    password:'123456',
     connectionLimit:10,
     database:'movie', // database
     port:'8306'
